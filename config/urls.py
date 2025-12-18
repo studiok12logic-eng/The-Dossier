d2e1217a41ec25ed03cf80e7226055892fb4ff6d
celkana <studio.k12logic@gmail.com>
2025-12-18 09:08:46 +0900

    Revert admin URL to admin/

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('shocola-brown/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
     path('accounts/', include('accounts.urls')),
     path('', include('core.urls')),
